3896d5925dd4705aaf0b1c9b9b006f795863475c
Douglas Jasper <doug.jasper@trios.com>
2026-08-24 08:34:34 -0400

    Modified build.gradle.kts to API 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.trios2025dej.locationfinder"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.trios2025dej.locationfinder"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
