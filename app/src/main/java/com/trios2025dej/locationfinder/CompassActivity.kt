34f3e34b950e75e1e5328adfc7d4f78a726cab8d
Douglas Jasper <doug.jasper@trios.com>
2026-08-25 09:29:02 -0400

    Added imports and variables to CompassActivity.kt

--- app/src/main/java/com/trios2025dej/locationfinder/CompassActivity.kt
+++ app/src/main/java/com/trios2025dej/locationfinder/CompassActivity.kt
@@ -5,8 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.widget.TextView
+import kotlin.math.roundToInt
 
 class CompassActivity : AppCompatActivity() {
+
+    private lateinit var sensorManager: SensorManager
+
+    private var accelerometer: Sensor? = null
+    private var magneticField: Sensor? = null
+
+    private lateinit var bearingText: TextView
+    private lateinit var directionText: TextView
+
+    private val gravity = FloatArray(3)
+    private val magnetic = FloatArray(3)
+
+    private val rotationMatrix = FloatArray(9)
+    private val orientation = FloatArray(3)
+
+    private var haveGravity = false
+    private var haveMagnetic = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
