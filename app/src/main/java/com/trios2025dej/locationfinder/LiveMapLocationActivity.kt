9b03ea80aa0e30ae40c81a85a3543f70894b51c9
Douglas Jasper <doug.jasper@trios.com>
2026-09-02 08:33:13 -0400

    Added variables to LiveMapLocationActivity.kt

--- app/src/main/java/com/trios2025dej/locationfinder/LiveMapLocationActivity.kt
+++ app/src/main/java/com/trios2025dej/locationfinder/LiveMapLocationActivity.kt
@@ -5,8 +5,55 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.os.Build
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+
+import org.maplibre.android.MapLibre
+import org.maplibre.android.annotations.Marker
+import org.maplibre.android.annotations.MarkerOptions
+import org.maplibre.android.camera.CameraUpdateFactory
+import org.maplibre.android.geometry.LatLng
+import org.maplibre.android.maps.MapLibreMap
+import org.maplibre.android.maps.MapView
+
+import java.util.Locale
 
 class LiveMapLocationActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var latitudeText: TextView
+    private lateinit var longitudeText: TextView
+    private lateinit var addressText: TextView
+    private lateinit var statusText: TextView
+
+    private lateinit var mapView: MapView
+
+    private lateinit var maplibreMap: MapLibreMap
+
+    private lateinit var locationRequest: LocationRequest
+
+    private lateinit var locationCallback: LocationCallback
+
+    private var currentLocationMarker: Marker? = null
+
+    private var mapIsReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
